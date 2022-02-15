288607a881e511525dcaa509a7d69cd44950fd68
NadielySilva <nady.ceng@gmail.com>
2022-02-14 22:32:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Map extends Component{
+  
+  state = {
+    dados: [
+      {
+        nome: "Leandro Murilo Fogaça",
+        idade: 46,
+        cpf: "734.828.905-36",
+        email: "leandromurilofogaca@ligananet.com.br"
+      },
+      {
+        nome: "Benício Noah Vieira",
+        idade: 52,
+        cpf: "749.003.785-93",
+        email: "benicionoahvieira_@mmarques.com"
+      },
+      {
+        nome: "Sebastião Raul Monteiro",
+        idade: 32,
+        cpf: "052.429.950-17",
+        email: "ssebastiaoraulmonteiro@eldor.it"
+      },
+      {
+        nome: "Camila Márcia Lívia Figueiredo",
+        idade: 21,
+        cpf: "923.932.834-39",
+        email: 'camilamarcialiviafigueiredo@zigotto.com.br'
+      },
+      {
+        nome: "Pedro Luan Ian Lima",
+        idade: 33,
+        cpf: "917.826.372-73",
+        email: "ppedroluanianlima@vivax.com.br"
+      },
+      {
+        nome: "Guilherme Martin Diogo Castro",
+        idade: 60,
+        cpf: "991.245.486-34",
+        email: "gguilhermemartindiogocastro@purkyt.com"
+      }]
+  };
+
+  render(){
+    return(
+      <div>
+        {this.state.dados.map((item) => (
+          <p>
+            Olá, {item.nome} o e-mail que você usou para login foi {item.email}
+          </p>
+        ) )}
+      </div>
+    );
+  }
 }
 
-export default App;
+//Crie um component de class
+
+//Crie um array de objetos no STATE com pelo menos 6 objetos
+
+//Faça um map do seu state e exiba os dados na tela
